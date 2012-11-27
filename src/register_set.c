26eec6afde8451ee6ce0c375c11a154d0505c77b
ericzhang <ericzhang.buaa@gmail.com>
2012-11-27 22:40:50 +0800

    register set bug fix and test case

--- src/register_set.c
+++ src/register_set.c
@@ -1,17 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include <math.h>
 #include "register_set.h"
 
-struct reg_set_t {
+struct reg_set_s {
     uint32_t count;
     uint32_t size;
     uint32_t M[1];
 };
 
-static const LOG2_BITS_PER_WORD = 6;
+static const uint32_t LOG2_BITS_PER_WORD = 6;
 
-static const REGISTER_SIZE = 5;
+static const uint32_t REGISTER_SIZE = 5;
 
 static uint32_t reg_set_getbits(uint32_t count)
 {
@@ -24,7 +26,7 @@ reg_set_t *rs_init(uint32_t count, uint32_t *values, uint32_t len)
     reg_set_t *rs;
 
     if (values) {
-        rs = (reg_set_t *)malloc(sizeof(reg_set_t) + len - 1);
+        rs = (reg_set_t *)malloc(sizeof(reg_set_t) + sizeof(uint32_t) * len - 1);
         memcpy(rs->M, values, len);
         rs->size = len;
     } else {
@@ -33,17 +35,19 @@ reg_set_t *rs_init(uint32_t count, uint32_t *values, uint32_t len)
             memset(rs->M, 0, 1);
             rs->size = 1;
         } else if (bits % 32 == 0) {
-            rs = (reg_set_t *)malloc(sizeof(reg_set_t) + bits - 1);
+            rs = (reg_set_t *)malloc(sizeof(reg_set_t) + sizeof(uint32_t) * bits - 1);
             memset(rs->M, 0, bits);
             rs->size = bits;
         } else {
-            rs = (reg_set_t *)malloc(sizeof(reg_set_t) + bits);
+            rs = (reg_set_t *)malloc(sizeof(reg_set_t) + sizeof(uint32_t) * bits);
             memset(rs->M, 0, bits + 1);
             rs->size = bits + 1;
         }
     }
 
     rs->count = count;
+
+    return rs;
 }
 
 int rs_set(reg_set_t *rs, uint32_t pos, uint32_t value)
@@ -67,7 +71,7 @@ int rs_get(reg_set_t *rs, uint32_t pos, uint32_t *value)
 
     uint32_t bucket_pos = (uint32_t)floor(pos / LOG2_BITS_PER_WORD);
     uint32_t shift = REGISTER_SIZE * (pos - (bucket_pos * LOG2_BITS_PER_WORD));
-    *value = (ctx->M[bucket_pos] & (0x1f << shift)) >> shift;
+    *value = (rs->M[bucket_pos] & (0x1f << shift)) >> shift;
 
     return 0;
 }
@@ -93,3 +97,6 @@ int rs_fini(reg_set_t *rs)
 
     return -1;
 }
+
+// vi:ft=c ts=4 sw=4 fdm=marker et
+
---
+++ t/register_set_unittest.c
@@ -0,0 +1,30 @@
+#include "register_set.h"
+#include "gtest/gtest.h"
+
+/**
+ * Tests set and get value of register set.
+ *
+ * <p>
+ * </p>
+ * */
+TEST(RegisterSetTest, GetAndSet) {
+    reg_set_t *rs = rs_init(1024, NULL, 0); 
+    uint32_t value;
+    int i;
+
+    for (i = 0; i < 1023; i++) {
+        rs_set(rs, i, i % 32);
+    }
+
+    for (i = 0; i < 1023; i++) {
+        if (i % 100 == 0) {
+            rs_get(rs, i, &value);
+            EXPECT_EQ(i % 32, value);
+        }
+    }
+
+    rs_fini(rs);
+}
+
+// vi:ft=c ts=4 sw=4 fdm=marker et
+
